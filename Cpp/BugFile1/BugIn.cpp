6ea5e9fe3f9f64d7a38839d5dfc3fe6ea7512828
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-09-17 22:49:01 +0530

    Create BugIn.cpp

---
+++ Cpp/BugFile1/BugIn.cpp
@@ -0,0 +1,29 @@
+    #include<iostream>
+    using namespace std;
+    class Cents{
+    	int m_cents;
+    	Cents(int cents){         // Private constructor
+    		m_cents=cents;
+    	}
+    	void print(){
+    		std::cout<<"I have "<<m_cents<<" cents\n";
+    	}
+    	friend Cents operator+(const Cents &c1,const Cents c2);
+    	int getCents(){
+    		return m_cents;
+    	}
+     
+    };
+    Cents operator+(const Cents &c1,const Cents &c2){
+    	return Cents(c1.m_cents+c2.m_cents);        // 'int Cents::m_cents' is private within this context 'return Cents(c1.m_cents+c2.m_cents);'
+    }
+     
+    int main(){
+    	Cents c1(6);
+    	Cents c2();         //erroneous object initialization
+    	Cents c3=c1+c2;
+    	c1.print();
+    	c2.print();
+    	c3.print();
+    	return 0;
+    }
