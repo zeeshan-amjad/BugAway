f86c26d9e089285f1909347464c221658307b3fc
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-09-17 23:45:22 +0530

    Create BugOut.c

---
+++ C/BugFile1/BugOut.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	int i,rows,space,k;
+ 	//printf("Enter the number of rows you wanted to print\n");
+ 	scanf("%d",&rows);
+    for(i=1;i<=rows;i++) {
+    	for(space=1;space<=rows-i;space++) 
+    		printf("  ");
+        for(space=1;space<=2*i-1;space++) 
+        	printf("* ");
+		printf("\n");
+    }
+	return 0;
+}
