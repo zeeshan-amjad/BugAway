b4ef4d3d5df052ca7fbe1123ad9d8b7182a0c0e4
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-09-17 23:44:25 +0530

    Create BugIn.c

---
+++ C/BugFile1/BugIn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>         // try not to use in online IDEs
+ 
+main(){
+int i,rows,space,k;
+ //printf("Enter the number of rows you wanted to print\n");
+ scanf("%d",&rows);
+        for(i=1;i<=rows;i++){
+            for(space=1;rows<=rows-i;space++) printf("   ");        // used wrong variable in condition check, printing 3 spaces instead of 2
+            for(space=1;space<=2*i-1;space++) printf("* ");
+ 
+printf("\n");
+        }
+ 
+getch ();         // try not to use in online IDEs
+return 0;
+}
