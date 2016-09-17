8b312541d42dc53a7fa4ef4f7ae72237f4c91e3b
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-09-17 22:49:38 +0530

    Create Bugout.cpp

---
+++ Cpp/BugFile1/Bugout.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Cents {
+	int m_cents;
+	public:
+	Cents(int cents = 0){
+		m_cents=cents;
+	}
+	void print(){
+		std::cout<<"I have "<<m_cents<<" cents\n";
+	}
+	friend Cents operator+(const Cents &c1,const Cents &c2);
+	int getCents(){
+		return m_cents;
+	}
+};
+Cents operator+(const Cents &c1,const Cents &c2){
+	Cents c (c1.m_cents+c2.m_cents);
+	return c;
+}
+
+int main(){
+	Cents c1(6);
+	Cents c2;
+	Cents c3=c1+c2;
+	c1.print();
+	c2.print();
+	c3.print();
+	return 0;
+}
