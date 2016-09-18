2b49d6014c40eb4dec2bc00fcb88e9cecd61d859
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-09-18 12:29:15 +0530

    Create BugIn.c

---
+++ C/BugFile2/BugIn.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main(){
+    //checking the given number is a perfect square
+    int a,n,c;							//flag variable that checks state is missing
+    printf("Enter any number \n");
+    scanf("%d",&n);		
+    for(a=0;a<=n;a++) {				
+															
+        if(n==a*a) printf("YEs the number is a perfect square");   // print statement showing a number is 'not' a perfect square is absent
+
+
+    }
+
+
+
+
+return 0;
+}
