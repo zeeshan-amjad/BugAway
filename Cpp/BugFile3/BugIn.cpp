50c3ab806a896b04c6448d4c703d106177fd1e68
Zeeshan Amjad <zeeshan.amzad@gmail.com>
2016-10-16 22:28:07 +0530

    Add files via upload

---
+++ Cpp/BugFile3/BugIn.cpp
@@ -0,0 +1,86 @@
+//programm to implement circular queue class
+#include<iostream>
+#define MIN -9999999
+using namespace std;
+class Queue{
+	int* m_data;
+	int m_front,m_rear;
+	int m_size;
+public:
+	Queue(int size){
+		m_data=new int[size];
+		m_front=-1;
+		m_rear=-1;
+		m_size=size;
+		for(int i=0;i<size;i++)
+			m_data[i]=MIN;
+		}
+	void add(int num){
+		if(m_front==-1&&m_rear==-1){
+			m_front=0;
+			m_rear=0;
+			m_data[m_rear]=num;
+			return;
+		}
+		else if(m_data[(m_rear+1)%(m_size)]!=MIN){
+			cout<<"Stack Overflow\n";
+			return;
+		}
+		else{
+			m_rear=(m_rear+1)%(m_size);
+			m_data[m_rear]=num;
+		}
+	}
+	int remove(){
+		if(m_data[m_front]==MIN){
+			return MIN;
+		}
+		int num=m_data[m_front];
+		m_data[m_front]=MIN;
+		m_front=(m_front+1)%(m_size);
+		return num;
+	}
+	void display(){
+		int front=m_front;
+		int rear=m_rear;
+		while(true){
+			if(m_data[front]==MIN)
+				break;
+			if(front==rear)         //position of this condition needs to be changed (think about the case when you are calling display() the first time)
+				break;
+			cout<<m_data[front]<<"\t";
+			front=(front+1)%(m_size);
+		}
+		cout<<endl;
+	}
+};
+
+int main(){
+	Queue q(5);
+	q.add(1);
+	q.display();
+	q.add(2);
+	q.display();
+	q.add(3);
+	q.display();
+	q.add(4);
+	q.display();
+	q.add(5);
+	q.display();
+	q.add(6);
+	q.display();
+	q.remove();
+	q.remove();
+	q.add(6);
+	q.display();
+	q.add(7);
+	q.display();
+	q.add(8);
+	return 0;
+}
+
+
+
+
+
+
